454723c222fefbba1b7b27fef8cbaaa81b100710
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-02 11:57:49 -0600

    se creo la ruta empleado al controlador, y dentro del controlador se agregron el return de la respectiva pagina index y create

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmpleadosController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Metodo para incluir las rutas hacia el controlador
+Route::resource('empleados', EmpleadosController::class);
+
+
