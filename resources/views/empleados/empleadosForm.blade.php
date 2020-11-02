bbf6ca9400be1618025fb7c9e5e0d333266f6982
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-02 15:34:58 -0600

    se bambio los label e input de empleadosCreate a empleadosForm para reutilzia el codigo, luego usamos include en create para mostrar los inputs del formulario

--- resources/views/empleados/empleadosForm.blade.php
+++ resources/views/empleados/empleadosForm.blade.php
@@ -1,1 +1,30 @@
-Formulario de datos del empleado
+
+{{ $Modo== 'crear' ? 'Agregar empleado':'Modificar empleado'}}
+
+<label for="Nombre">{{'Nombre'}}</label>
+<input type="text" name="Nombre" id="Nombre" value="">
+<br/>
+
+
+<label for="ApellidoPaterno">{{'Apellido Paterno'}}</label>
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="">
+<br/>
+
+
+<label for="ApellidoMaterno">{{'Apellido Materno'}}</label>
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="">
+<br/>
+
+
+<label for="Correo">{{'Correo'}}</label>
+<input type="email" name="Correo" id="Correo" value="">
+<br/>
+
+
+<label for="Foto">{{'Foto'}}</label>
+<input type="file" name="Foto" id="Foto" value="">
+<br/>
+
+
+<input type="submit" value="Agregar">
+<a href="{{ url('empleados') }}">Regresar</a>
