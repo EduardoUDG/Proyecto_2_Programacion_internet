b99677311ffa2c00161e52978e76d693e8f787e5
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-02 16:00:57 -0600

    se dejo un include en empleadosEdit y en empleadosForm se agregaron las condiciones a mostrar, en caso de que el formulario agrege o modifique

--- resources/views/empleados/empleadosForm.blade.php
+++ resources/views/empleados/empleadosForm.blade.php
@@ -2,29 +2,38 @@
 {{ $Modo== 'crear' ? 'Agregar empleado':'Modificar empleado'}}
 
 <label for="Nombre">{{'Nombre'}}</label>
-<input type="text" name="Nombre" id="Nombre" value="">
+<input type="text" name="Nombre" id="Nombre"
+value="{{ isset($empleado->Nombre)?$empleado->Nombre:'' }}">
 <br/>
 
 
 <label for="ApellidoPaterno">{{'Apellido Paterno'}}</label>
-<input type="text" name="ApellidoPaterno" id="ApellidoPaterno" value="">
+<input type="text" name="ApellidoPaterno" id="ApellidoPaterno"
+value="{{ isset($empleado->ApellidoPaterno)?$empleado->ApellidoPaterno:'' }}">
 <br/>
 
 
 <label for="ApellidoMaterno">{{'Apellido Materno'}}</label>
-<input type="text" name="ApellidoMaterno" id="ApellidoMaterno" value="">
+<input type="text" name="ApellidoMaterno" id="ApellidoMaterno"
+value="{{ isset($empleado->ApellidoMaterno)?$empleado->ApellidoMaterno:'' }}">
 <br/>
 
 
 <label for="Correo">{{'Correo'}}</label>
-<input type="email" name="Correo" id="Correo" value="">
+<input type="email" name="Correo" id="Correo"
+value="{{ isset($empleado->Correo)?$empleado->Correo:'' }}">
 <br/>
 
 
 <label for="Foto">{{'Foto'}}</label>
+@if (isset($empleado->Foto))
+<br/>
+<img src="{{ asset('storage').'/'.$empleado->Foto }}" alt="" width="200">
+<br/>
+@endif
 <input type="file" name="Foto" id="Foto" value="">
 <br/>
 
 
-<input type="submit" value="Agregar">
+<input type="submit" value="{{ $Modo== 'crear' ? 'Agregar':'Modificar'}}">
 <a href="{{ url('empleados') }}">Regresar</a>
