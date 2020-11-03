10c26518757771e83e6aafc98f6b27f9b3fe5a2b
Eduardo Chavez <eduardo.cdorado@alumnos.udg.mx>
2020-11-02 21:04:54 -0600

    se copio @section para crear una vista un poco mas bonita en empleadosIndex, empleadosEdit y empleadosCreate

--- resources/views/empleados/empleadosCreate.blade.php
+++ resources/views/empleados/empleadosCreate.blade.php
@@ -1,10 +1,16 @@
-Seccion para crear empleaods
+@extends('layouts.app')
 
+@section('content')
+
+<div class="container">
+Seccion para crear empleados
 
 <form action="{{ url('/empleados') }}" method="post" enctype="multipart/form-data">
+
 {{ csrf_field() }}
 @include('empleados.empleadosForm',['Modo'=>'crear'])
 
-
-
 </form>
+</div>
+
+@endsection
--- resources/views/empleados/empleadosIndex.blade.php
+++ resources/views/empleados/empleadosIndex.blade.php
@@ -1,55 +1,59 @@
+@extends('layouts.app')
 
-@if (Session::has('Mensaje')){{
-    Session::get('Mensaje')
-}}
-@endif
+@section('content')
+
+<div class="container">
+    @if (Session::has('Mensaje')){{
+        Session::get('Mensaje')
+    }}
+    @endif
+
+    <a href="{{ url('empleados/create') }}">Agregar Empleado</a>
+    <table class="table table-light">
+        <thead class="thead-light">
+            <tr>
+                <th>ID</th>
+                <th>Foto</th>
+                <th>Nombre</th>
+                <th>Apellido Paterno</th>
+                <th>Apellido Materno</th>
+                <th>Correo</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+
+        <tbody>
+        @foreach ($empleados as $empleado)
+            <tr>
+                <td>{{$loop->iteration}}</td>
+                <td>
+                <img src="{{ asset('storage').'/'.$empleado->Foto }}" alt="" width="200">
+                </td>
+                <td>{{ $empleado->Nombre }}</td>
+                <td>{{ $empleado->ApellidoPaterno }}</td>
+                <td>{{ $empleado->ApellidoMaterno }}</td>
+                <td>{{ $empleado->Correo }}</td>
+                <td>
+
+                    <a href="{{ url('/empleados/'.$empleado->id.'/edit') }}">
+                    Editar
+                    </a>
+
+                    |
+
+                <form action="{{ url('/empleados/'.$empleado->id) }}" method="post">
+                    {{ csrf_field() }}
+                    {{ method_field('DELETE') }}
+                    <button type="submit" onclick="return confirm('¿Borrar?');">Borrar</button>
+                </form>
 
 
+                </td>
+            </tr>
+        @endforeach
+        </tbody>
 
-<a href="{{ url('empleados/create') }}">Agregar Empleado</a>
+    </table>
+</div>
 
-<table class="table table-light">
-    <thead class="thead-light">
-        <tr>
-            <th>ID</th>
-            <th>Foto</th>
-            <th>Nombre</th>
-            <th>Apellido Paterno</th>
-            <th>Apellido Materno</th>
-            <th>Correo</th>
-            <th>Acciones</th>
-        </tr>
-    </thead>
-
-    <tbody>
-    @foreach ($empleados as $empleado)
-        <tr>
-            <td>{{$loop->iteration}}</td>
-            <td>
-            <img src="{{ asset('storage').'/'.$empleado->Foto }}" alt="" width="200">
-            </td>
-            <td>{{ $empleado->Nombre }}</td>
-            <td>{{ $empleado->ApellidoPaterno }}</td>
-            <td>{{ $empleado->ApellidoMaterno }}</td>
-            <td>{{ $empleado->Correo }}</td>
-            <td>
-
-                <a href="{{ url('/empleados/'.$empleado->id.'/edit') }}">
-                Editar
-                </a>
-
-                |
-
-            <form action="{{ url('/empleados/'.$empleado->id) }}" method="post">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit" onclick="return confirm('¿Borrar?');">Borrar</button>
-            </form>
-
-
-            </td>
-        </tr>
-    @endforeach
-    </tbody>
-
-</table>
+@endsection
